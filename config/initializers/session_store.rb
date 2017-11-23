204665b9d41a3adc296583442654277ff7773857
William Young <william@werl.me>
2017-11-23 01:58:58 -0600

    Change to redis sessions store
    This will provide a more secure session store than cookie so that
    vertical/horizontal permission changes should not be easily possible

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_taskzero_session'
+Rails.application.config.session_store :redis_session_store, {
+  key: '_taskzero-key',
+  redis: {
+    expire_after: 120.minutes,
+    key_prefix: 'taskzero:session:',
+    url: 'redis://127.0.0.1'
+  }
+}
