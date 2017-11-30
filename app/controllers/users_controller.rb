c3b2c2876662f9f1ce367edd1af10dfd4deff97b
William Young <william@werl.me>
2017-11-30 04:17:31 -0600

    Fixes #7 and more
    Fixes #11
    Fixes #12
    Fixes #13
    
    Feature Envy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,11 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      parameters = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-      if parameters[:password]== '' and parameters[:password_confirmation] == ''
-        parameters.delete(:password)
-        parameters.delete(:password_confirmation)
-      end
-      return parameters
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
