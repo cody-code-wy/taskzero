1a69451e7cb3e22f49051485ff36950ad65a11c3
William Young <william@werl.me>
2017-11-30 07:16:36 -0600

    Switch to using SimpleCov's rails integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ RSpec.configure do |config|
   require 'simplecov'
 
   # Simple Cov will produce code coverage results
-  SimpleCov.start
+  SimpleCov.start 'rails'
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
