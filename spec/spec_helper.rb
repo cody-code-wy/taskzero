803043ccd166e3ae36bfb45178572d42465d2345
William Young <william@werl.me>
2017-12-02 04:16:13 -0600

    Filter out application_cable from testing coverage
    These do not have an implementation, or any way to test yet.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,10 @@ RSpec.configure do |config|
   require 'simplecov'
 
   # Simple Cov will produce code coverage results
-  SimpleCov.start 'rails'
+  SimpleCov.start 'rails' do
+    add_filter 'app/channels/application_cable/channel.rb'
+    add_filter 'app/channels/application_cable/connection.rb'
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
